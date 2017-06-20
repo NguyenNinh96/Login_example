fe71921f4a18fdab0bd94e19b7153c740c71aab9
Nguyen Ninh <ninh.nt@anlab.pro>
2017-06-20 13:45:16 +0700

    login

---
+++ input.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html>
+	<body>
+		<form action="welcome.php" method="post">
+			Username:<input type="text" name="username"><br>
+			Password:<input type="password" name="password"><br>
+			 <input type="submit" name="connect" value="login">
+		</form>
+		
+	</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,20 @@
+﻿<?php
+	 $u=$_POST['username'];
+	 $p=$_POST['password'];
+	$con=mysql_connect("localhost","root","") or die ("no connect");
+	mysql_select_db("vidu_1");
+	mysql_query("set names 'utf8'");
+	$sql = "select * from input";
+	$query = mysql_query($sql);
+	$check=false;
+	while($row = mysql_fetch_array($query)){
+		if( $row["Username"]==$u && $row["Password"]==$p)
+			$check = true;
+	}
+	if($check == true)
+		echo "Welcome, ".$u;
+	else
+		echo "User not found!!";
+	mysql_close($con);			
+
+?>
