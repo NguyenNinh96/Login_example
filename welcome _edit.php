f3de7f99e8c651d12e3fde4858cbbd3309ecb6fa
Nguyen Ninh <ninh.nt@anlab.pro>
2017-06-20 14:33:55 +0700

    login

---
+++ welcome _edit.php
@@ -0,0 +1,23 @@
+﻿<?php
+	 $u=$_POST['username'];
+	 $p=$_POST['password'];
+	$con=mysql_connect("localhost","root","") or die ("no connect");
+	mysql_select_db("vidu_1");
+	mysql_query("set names 'utf8'");
+	$sql = "select * from input where Username='".$u."' and Password='".$p."'";
+	$query = mysql_query($sql);
+	$check=false;
+	//while($row = mysql_fetch_array($query)){
+		//if( $row["Username"]==$u && $row["Password"]==$p)
+		//	$check = true;
+	//}
+	if(mysql_fetch_array($query) != NULL){
+		$check=true;
+	}
+	if($check == true)
+		echo "Welcome, ".$u;
+	else
+		echo "User not found!!";
+	mysql_close($con);			
+
+?>
