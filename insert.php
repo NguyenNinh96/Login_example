6c5a502dd027b1bc2c1828051a0a921b8a32fb91
Nguyen Ninh <ninh.nt@anlab.pro>
2017-06-23 11:35:43 +0700

    kiem tra dang ky

--- insert.php
+++ insert.php
@@ -1,19 +1,20 @@
-﻿<?php
-	 $u=$_POST['username'];
-	 $p=$_POST['password']; 
-	$r=$_POST['repw'];
-	if($u==NULL || $p==NULL || $r==NULL){
-		echo "Nhap khong hop le";
-	} else if($p != $r){
-		echo "Nhap sai";
-	}else
-	{
-	$con=mysql_connect("localhost","root","") or die ("no connect");
-	mysql_select_db("vidu_1");
-	mysql_query("set names 'utf8'");
-	$sql = "INSERT INTO `input` ( `Username`, `Password`) VALUES ( '$u','$p');";
-	$query = mysql_query($sql);
-	echo"dang ky thanh cong";
-	mysql_close($con);
+﻿<?php 
+	if(isset($_POST['username'])){
+		$u=$_POST['username'];
+		$p=$_POST['password'];
+		$con=mysql_connect("localhost","root","") or die ("no connect");
+		mysql_select_db("vidu_1");
+		mysql_query("set names 'utf8'");
+		$query = mysql_query("SELECT * from input where Username = '".$u."'");
+		$result = mysql_num_rows($query);
+		
+		if ($result > 0){
+			echo "trung";
+		}
+		else{
+			echo "thanhcong";
+			mysql_query("INSERT INTO `input` ( `Username`, `Password`) VALUES ( '$u','$p');");
+		}
+		mysql_close($con);
 	}
 ?>
--- sign_up.php
+++ sign_up.php
@@ -1,11 +1,118 @@
 ﻿<!DOCTYPE html>
 <html>
+	<head>
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+<style>
+ 
+</style> 
+<script
+ src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"
+ type="text/javascript"></script>
+<script
+ src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"
+ type="text/javascript"></script>
+<script
+ src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.js"
+ type="text/javascript"></script>
+<script src="/resources/scripts/mysamplecode.js" type="text/javascript"></script>
+<script src="/resources/scripts/date.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$('document').ready(function()
+{
+    $("#a").validate({
+        rules:
+        {
+            username: {
+                required: true
+            },
+            password: {
+                required: true,
+                minlength: 4
+            },
+            repw: {
+                required: true,
+                equalTo: '#pass'
+            },
+        },
+        messages:
+        {
+            username: "<br> Chưa nhập tên",
+            password:{
+                required: "<br> Chưa nhập mật khẩu",
+                minlength: "<br> Mật khẩu phải trên 4 ký tự"
+            },
+            repw:{
+                required: "<br> Chưa xác nhận lại mật khẩu",
+                equalTo: "<br> Mật khẩu chưa trùng nhau"
+            }
+        },
+        submitHandler: submitForm
+    });
+    
+    function submitForm()
+    {
+        var data = $("#a").serialize();
+
+        $.ajax({
+
+            type : 'POST',
+            url  : 'insert.php',
+            data : data,
+            beforeSend: function()
+            {
+                $("#error").fadeOut();
+            },
+            success :  function(data)
+            {
+                if(data=="trung"){
+
+                    $("#error").fadeIn(1000, function(){
+
+
+                        $("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; Ten da ton tai !</div>');
+                    });
+                }
+                else if(data=="thanhcong")
+                {
+
+                    setTimeout('$(".sign").fadeOut(500, function(){ $(".form").html("Thanh cong"); }); ',5000);
+
+                }
+                else{
+
+                    $("#error").fadeIn(1000, function(){
+                        $("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+data+' !</div>');
+
+                    });
+
+                }
+            }
+        });
+        return false;
+    }
+});
+</script>
+	</head>
 	<body>
-		<form action="insert.php" method="post">
-			Username:<input type="text" name="username"><br>
-			Password:<input type="password" name="password"><br>
-			Reconfirm password:<input type="password" name="repw"><br>
-			 <input type="submit" name="connect" value="sign-up">
+	<div class="form">
+		<form id= "a"action="insert.php" method="post" name="a" class="sign">
+			<div>
+			Username:<input id="username" type="text" name="username"><br>
+			</div>
+			<div id="error"></div>
+			<div>
+			Password:<input id="pass" type="password" name="password"><br>
+			</div>
+			<div>
+			Reconfirm password:<input id="repass" type="password" name="repw"><br>
+			</div>
+			 <button type="submit" name="connect" value="sign-up" id="submit">
+			 Login
+			 </button>
 		</form>
+	
+	</div>
 	</body>
+	
 </html>
